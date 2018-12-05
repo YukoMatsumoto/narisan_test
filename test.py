ca75c97c6352e522828ea1557369ce0ed763b728
yu-rashia <uajag4y@gmail.com>
2018-12-05 15:07:33 +0900

    正しいパスワードを入力したらtest.htmlページに飛ぶようになった

--- test.py
+++ test.py
@@ -1,16 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/login", methods=["GET"])
 def index():
     return render_template("login2.html")
 
 
 @app.route("/login_action", methods=["POST"])
 def login():
-    return render_template("test.html")
+    if request.method == "POST":
+        password = request.form["password"]
+        if password == "password":
+            return render_template("test.html")
+        else:
+            return render_template("login2.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
